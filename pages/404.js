438dd25f11cb5c8e8542d18fb09199ace55477ea
Abdul-Razak <arazak4r@gmail.com>
2021-02-24 11:34:14 +0100

    redirecting from 404 page to homepage

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,22 @@
 import Link from "next/Link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+  //Initialize userouter hook
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      // router.go(-1)
+      router.push("/");
+    }, 3000);
+  }, []);
   return (
     <div className="not-found">
       <h1>Oooops...</h1>
       <h2>That page cannot be found.</h2>
       <p>
-        Go back to the{" "}
+        Go back to the
         <Link href="/">
           <a>Homepage</a>
         </Link>
