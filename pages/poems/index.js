6c04b55c073df64fdfc525d1104de8f6543d4b00
Abdul-Razak <arazak4r@gmail.com>
2021-02-24 13:17:56 +0100

    fetch data at build time with getStaticProps

--- pages/poems/index.js
+++ pages/poems/index.js
@@ -1,7 +1,26 @@
-const Friends = () => {
+//runs at build time
+import styles from "../../styles/Poems.module.module.css";
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { names: data },
+  };
+};
+
+const Friends = ({ names }) => {
+  console.log(names);
   return (
     <div>
-      <h2>friends</h2>
+      <h2>Poets</h2>
+      {names.map((friend) => (
+        <div key={friend.id}>
+          <a className={styles.single}>
+            <h3>{friend.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
